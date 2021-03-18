14b62320f0649c599938014b3c8cd5ba1b572f69
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2021-03-19 00:59:02 +0900

    標準のファイル・ディレクトリ操作ライブラリの使用

---
+++ 13_handle_files_and_directories.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <filesystem>
+using namespace std;
+namespace fs = std::filesystem;
+
+int main() {
+    // カレントディレクトリを取得
+    cout << fs::current_path() << endl;
+
+    // カレントディレクトリを設定
+    fs::current_path("E:\\Cpp20_for_game_developers");
+    cout << fs::current_path() << endl;
+
+    // ファイルが存在するか否かを確認
+    if (fs::exists("test.txt")) {
+        fs::copy("test.txt", "test2.txt"); // ファイルをコピー
+    }
+    // ファイルを読み込む前に、ファイルのサイズを取得
+    cout << fs::file_size("save.dat") << endl;
+
+    // フォルダを作成
+    fs::create_directories("aaa/bbb/ccc");
+
+    // カレントディレクトリに含まれる全ファイルを出力
+    for (const auto& entry : fs::recursive_directory_iterator("./")) {
+        cout << entry.path() << endl;
+    }
+}
