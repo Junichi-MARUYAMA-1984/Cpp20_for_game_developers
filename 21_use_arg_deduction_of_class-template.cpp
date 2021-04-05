ce54514eb4cab1663650b02ddd271305a9b1190c
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2021-04-05 10:02:03 +0900

    21番について、std::stringリテラル演算子sの代わりに明示的に型を指定するよう修正した。

--- 21_use_arg_deduction_of_class-template.cpp
+++ 21_use_arg_deduction_of_class-template.cpp
@@ -4,16 +4,11 @@
 #include <tuple>
 #include <mutex>
 
-std::string operator"" s(const char* str, std::size_t length)
-{
-  return std::string(str, length);
-}
-
 // C++14
 void F() {
     std::vector<int>               v = { 1, 2, 3 };
     std::array<double, 3>          a = { 0.1, 0.2, 0.3 };
-    std::tuple<int, float, std::string> t = { 30, 0.5f, "hello"s };
+    std::tuple<int, float, std::string> t = { 30, 0.5f, "hello" };
     std::mutex m;
     std::lock_guard<std::mutex> lock(m);
 }
@@ -22,18 +17,18 @@ void F() {
 void G() {
     std::vector v = { 1, 2, 3 };
     std::array  a = { 0.1, 0.2, 0.3 };
-    std::tuple  t = {30, 0.5f, "hello"s };
+    std::tuple  t = {30, 0.5f, std::string("hello") };
     std::mutex m;
     std::lock_guard lock(m);
 }
 
 int main() {
     // テンプレート引数を省略しない場合
-    std::tuple<int, float, std::string> t1 = { 30, 0.5f, "hello"s };
+    std::tuple<int, float, std::string> t1 = { 30, 0.5f, "hello" };
 
     // 変数の型を推論させ、代わりに生成関数を使う場合
-    auto t2 = std::make_tuple(30, 0.5f, "hello"s);
+    auto t2 = std::make_tuple(30, 0.5f, std::string("hello"));
 
     // テンプレート引数を省略する場合
-    std::tuple t3 = { 30, 0.5f, "hello"s };
+    std::tuple t3 = { 30, 0.5f, std::string("hello") };
 }
