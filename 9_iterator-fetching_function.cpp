0b85d6ead64e78b5ba3e72ac2c615a443de7312b
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2021-04-20 17:09:12 +0900

    9: 処理内容の出力処理を追加

--- 9_iterator-fetching_function.cpp
+++ 9_iterator-fetching_function.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -7,12 +8,30 @@ void F(Container& c) {
     size_t n = size(c);
     int64_t i = ssize(c); // サイズを符号付き整数型で得たい場合に便利。
     fill(begin(c), end(c), 0);
+    cout << "size(c) = " << n << ", ssize(c) = " << i << endl;
 }
 
 int main() {
     vector<int> v = { 1, 2, 3 };
     int a[4] = { 10, 20, 30, 40 };
 
+    for (auto e : v) {
+        cout << e << endl;
+    }
+
     F(v); // OK
+
+    for (auto e : v) {
+        cout << e << endl;
+    }
+
+    for (auto e : a) {
+        cout << e << endl;
+    }
+
     F(a); // OK
+
+    for (auto e : a) {
+        cout << e << endl;
+    }
 }
